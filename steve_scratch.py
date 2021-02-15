1303e79fa8e98263e3b66d0006e49cd6bcde394b
stekim <stevekim121@gmail.com>
2021-02-15 14:58:22 -0800

    Update steve_scratch.py

--- steve_scratch.py
+++ steve_scratch.py
@@ -8,16 +8,29 @@ Created on Sun Feb 14 01:28:40 2021
 # import numpy as np
 import pandas as pd
 
-gdp_2016 = pd.read_csv('data/2016.csv')
-energy = pd.read_csv('data/energy.csv')
-gender = pd.read_csv('data/gender_data.csv')
+gdp_2016 = pd.read_csv('h/2016.csv')
+energy = pd.read_csv('h/energy.csv')
+gender = pd.read_csv('h/gender_data.csv')
 
 
 energy = energy.drop(columns=['Code'])
 energy = energy.set_index('Year')
 line = energy.plot.line()
 
-hydro = energy['Entity','Hydropower (terawatt-hours)']
+hydro = energy[['Entity','Hydropower (terawatt-hours)']]
+top_20_countries = hydro.groupby('Entity').sum().sort_values('Hydropower (terawatt-hours)', ascending=False)[1:20].index.values
 h = hydro.plot.line()
+
+solar = energy[['Entity','Solar (terawatt-hours)']]
+top_20_countries = solar.groupby('Entity').sum().sort_values('Solar (terawatt-hours)', ascending=False)[1:20].index.values
+s = solar.plot.line()
+
+wind = energy[['Entity','Wind (terawatt-hours)']]
+top_20_countries = wind.groupby('Entity').sum().sort_values('Wind (terawatt-hours)', ascending=False)[1:20].index.values
+w = wind.plot.line()
+
+other = energy[['Entity','Other renewables (terawatt-hours)']]
+top_20_countries = other.groupby('Entity').sum().sort_values('Other renewables (terawatt-hours)', ascending=False)[1:20].index.values
+o = other.plot.line()
 # ax = energy.plot.bar(x='Entity', y=['Solar (terawatt-hours)'], rot=0)
 
