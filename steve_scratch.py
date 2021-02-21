c2ebaa37e3ab69d292f362e7bcbe1e4d75425225
stekim <stevekim121@gmail.com>
2021-02-21 14:03:18 -0800

    Update steve_scratch.py

--- steve_scratch.py
+++ steve_scratch.py
@@ -8,10 +8,14 @@ Created on Sun Feb 14 01:28:40 2021
 # Energy
 # import numpy as np
 import pandas as pd
+import missingno as msno
 
-gdp_2016 = pd.read_csv('h/2016.csv')
 energy = pd.read_csv('h/energy.csv')
-gender = pd.read_csv('h/gender_data.csv')
+gdp_2016 = pd.read_csv('h/2016.csv')
+# gender = pd.read_csv('h/gender_data.csv')
+# %matplotlib inlines
+msno.matrix(energy.sample(250))
+msno.matrix(gdp_2016.sample(150))
 
 
 energy = energy.drop(columns=['Code'])
@@ -19,27 +23,29 @@ energy = energy.set_index('Year')
 line = energy.plot.line()
 
 hydro = energy[['Entity','Hydropower (terawatt-hours)']]
-top_20_countries = hydro.groupby('Entity').sum().sort_values('Hydropower (terawatt-hours)', ascending=False)[1:20].index.values
+top_10_countries = hydro.groupby('Entity').sum().sort_values('Hydropower (terawatt-hours)', ascending=False)[1:10].index.values
+hydro = hydro[hydro['Entity'].isin(top_10_countries)]
 h = hydro.plot.line()
 
 solar = energy[['Entity','Solar (terawatt-hours)']]
-top_20_countries = solar.groupby('Entity').sum().sort_values('Solar (terawatt-hours)', ascending=False)[1:20].index.values
+top_10_countries = solar.groupby('Entity').sum().sort_values('Solar (terawatt-hours)', ascending=False)[1:10].index.values
+solar = solar[solar['Entity'].isin(top_10_countries)]
 s = solar.plot.line()
 
 wind = energy[['Entity','Wind (terawatt-hours)']]
-top_20_countries = wind.groupby('Entity').sum().sort_values('Wind (terawatt-hours)', ascending=False)[1:20].index.values
+top_10_countries = wind.groupby('Entity').sum().sort_values('Wind (terawatt-hours)', ascending=False)[1:10].index.values
+wind = wind[wind['Entity'].isin(top_10_countries)]
 w = wind.plot.line()
 
 other = energy[['Entity','Other renewables (terawatt-hours)']]
-top_20_countries = other.groupby('Entity').sum().sort_values('Other renewables (terawatt-hours)', ascending=False)[1:20].index.values
+top_10_countries = other.groupby('Entity').sum().sort_values('Other renewables (terawatt-hours)', ascending=False)[1:10].index.values
+other = other[other['Entity'].isin(top_10_countries)]
 o = other.plot.line()
 # ax = energy.plot.bar(x='Entity', y=['Solar (terawatt-hours)'], rot=0)
 
 
 ###################################
 # Womens Rights
-
-rights = pd.read_csv('gender_data.csv')
 
 
 
