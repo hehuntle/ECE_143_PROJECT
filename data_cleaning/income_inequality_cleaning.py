3558ae038cacfa0066cebc1960048d09130d100e
stekim <stevekim121@gmail.com>
2021-03-08 20:28:41 -0800

    Update income_inequality_cleaning.py

--- data_cleaning/income_inequality_cleaning.py
+++ data_cleaning/income_inequality_cleaning.py
@@ -8,10 +8,11 @@ from scipy.stats import pearsonr
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
-data = pd.read_csv("incomeinequality.csv")
-
-data['GDP per capita, PPP (current international $) [NY.GDP.PCAP.PP.CD]'].replace('..', np.nan, inplace=True)
-data['Income share held by highest 10% [SI.DST.10TH.10]'].replace('..', np.nan, inplace=True)
-data.dropna(subset=['Income share held by highest 10% [SI.DST.10TH.10]'], inplace=True)
-data['Income of Top 10%'] = data['Income share held by highest 10% [SI.DST.10TH.10]'].astype(float)
-data['GDP Per Capita'] = data['GDP per capita, PPP (current international $) [NY.GDP.PCAP.PP.CD]'].astype(float)
+def income_disparity_clean(filename):
+  data = pd.read_csv(filename)
+  data['GDP per capita, PPP (current international $) [NY.GDP.PCAP.PP.CD]'].replace('..', np.nan, inplace=True)
+  data['Income share held by highest 10% [SI.DST.10TH.10]'].replace('..', np.nan, inplace=True)
+  data.dropna(subset=['Income share held by highest 10% [SI.DST.10TH.10]'], inplace=True)
+  data['Income of Top 10%'] = data['Income share held by highest 10% [SI.DST.10TH.10]'].astype(float)
+  data['GDP Per Capita'] = data['GDP per capita, PPP (current international $) [NY.GDP.PCAP.PP.CD]'].astype(float)
+  return data
