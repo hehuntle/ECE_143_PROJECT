bed7b7a2355eefcee0717843c93cc5f3b9e8b2a7
stekim <stevekim121@gmail.com>
2021-03-08 20:22:29 -0800

    Add files via upload

---
+++ data_cleaning/income_inequality_cleaning.py
@@ -0,0 +1,17 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import OrderedDict
+from collections import defaultdict
+from scipy.stats import pearsonr
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+data = pd.read_csv("incomeinequality.csv")
+
+data['GDP per capita, PPP (current international $) [NY.GDP.PCAP.PP.CD]'].replace('..', np.nan, inplace=True)
+data['Income share held by highest 10% [SI.DST.10TH.10]'].replace('..', np.nan, inplace=True)
+data.dropna(subset=['Income share held by highest 10% [SI.DST.10TH.10]'], inplace=True)
+data['Income of Top 10%'] = data['Income share held by highest 10% [SI.DST.10TH.10]'].astype(float)
+data['GDP Per Capita'] = data['GDP per capita, PPP (current international $) [NY.GDP.PCAP.PP.CD]'].astype(float)
